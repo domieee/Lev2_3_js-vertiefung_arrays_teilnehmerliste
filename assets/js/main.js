41785a3156eb852736a31eb6a7c056dddeb198ac
domieee <99103235+domieee@users.noreply.github.com>
2023-01-31 14:34:08 +0100

    changed dir structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,50 +5,55 @@ const table = document.querySelector('.table')
 const participantArray = []
 /* 
 TODOS:
-    ! - Create possibility to append a participant
-        ! - Create a function that creates a new table row for each participant
-        ! - Each participant needs a specific id to control them separately
-    - Create a possibilty to delete a participant from the table
-        - Create a function to select a participant
-            - Change the background color of the row
-            - Save the specitic ID of the selected Item in a variable
-        - Create a function to delete a participant
-            - Create a statement to pop the last Item if no other Item is selected
+     - Create possibility to append a participant
+         - Create a function that creates a new table row for each participant
+         - Each participant needs a specific id to control them separately
+    TODO: - Create a possibilty to delete a participant from the table
+        TODO: - Create a function to select a participant
+            TODO: - Add a class to every selected participant
+        - Create a function to delete every selected participant
+            - Create a statement to pop the last Item if no other item is selected
 */
-
 
 function createParticipantTable() {
     // Creates a new table row and adds a specific id for each new row
     let row = table.insertRow();
     row.setAttribute('id', 'row-' + participantArray.length);
 
-
+    // Creates new table cells and them to the table row
     let cellNr = row.insertCell();
     let cellName = row.insertCell();
 
+    // ? Give the cells  
     cellNr.innerHTML = participantArray.length + '.';
     cellName.innerHTML = participant.value;
 
+    // Push the element in an array
     participantArray.push(cellName.innerHTML);
 
-    var elements= document.getElementsByTagName('tr');
-
-    for (var i = 0; i <= participantArray.length; i++) {
-        (elements)[i].addEventListener("click", function () {
-            let selectedIndexNumber = this.id;
-            selectParticipant(selectedIndexNumber);
-        });
-    }
+    row.addEventListener("click", () => {
+        console.log('object');
+        // let selectedIndexNumber = this.id;
+        // console.log(selectedIndexNumber);
+        // selectParticipant(selectedIndexNumber);
+    });
 }
 
-function selectParticipant(number) {
-    let selectedParticipant = document.getElementById(number);
+// function selectParticipant(number) {
+//     let selectedParticipant = document.getElementById(number);
+//     selectedParticipant.classList.add('.selected')
+// }
 
-    selectedParticipant.style.backgroundColor = '#0000ff'
-    selectedParticipant.style.color = '#fff'
-}
+// Adds a EventsListener to every table row
+    // If one item gets clicked, the function selectParticipant() is called and the corresponding id trasmitted
+// let elements = document.getElementsByTagName('tr');
+// elements[0].addEventListener('click', () => {
+//     elements[0].classList.toggle('selected');
+// })
+// for (var i = 0; i <= participantArray.length; i++) {
 
-document.addEventListener('click', selectParticipant)
-push.addEventListener('click', createParticipantTable)
+// }
+
+push.addEventListener('click', createParticipantTable);
 
 
